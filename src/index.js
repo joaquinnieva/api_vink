e4fd562074008519c15c4fd39ab9e8ebe2715a0f
joaquinnieva <joacoo.nieva@gmail.com>
2022-02-20 05:28:34 -0300

    fix file limit

--- src/index.js
+++ src/index.js
@@ -12,10 +12,8 @@ app.set('port', process.env.PORT || 3001);
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, parameterLimit: 50000 }));
+app.use(cors());
 
 // Routes
 app.use('/api/user', require('./routes/User.routes'));
